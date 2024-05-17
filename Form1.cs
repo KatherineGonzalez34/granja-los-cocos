fa4c7579afa7516f3978d928627ec7d3a66a39fe
KathGonzalez <katy06salinas@gmail.com>
2024-05-17 00:47:21 -0600

    Conexion a base de datos de prueba

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,33 @@ namespace GranjaLosCocos
 {
     public partial class Form1 : Form
     {
+        private Conexion conexion;
         public Form1()
         {
             InitializeComponent();
+            conexion = new Conexion();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string result = "";
+            //MySqlDataReader mySqlDataReader = null;
+            //string consulta = "SELECT * FROM alumnos";
+            if(conexion.getConexion() != null)
+            {
+                //MySqlCommand mySqlCommand = new MySqlCommand(consulta);
+                //mySqlCommand.Connection = conexion.getConexion();
+                //mySqlDataReader = mySqlCommand.ExecuteReader();
+                //while (mySqlDataReader.Read())
+                //{
+                    //result = mySqlDataReader.GetString("nombre");
+                //
+
+                MessageBox.Show("Conexion establecida correctamente");
+            } else
+            {
+                MessageBox.Show("No hay conexion establecida correctamente");
+            }
         }
     }
 }
